18efb6fda5d0f95984a6c5ffb7c049142b7eca3b
日比野大輝 <hibinodaiki@hibinodaikinoMacBook-Air.local>
2023-06-02 16:20:17 +0900

    feat/es6への変更、画像パスの変数格納

--- script.js
+++ script.js
@@ -1,33 +1,37 @@
 // wait()を使用すると2秒待機
-function wait(seconds) {
+const wait = (seconds) => {
   return new Promise((resolve) => {
     setTimeout(() => {
       resolve();
     }, seconds * 1000);
   });
-}
+};
 
-function createImage(imagePath) {
+const createImage = (imagePath) => {
+  // まずはhtmlのbodyを指定
+  const body = document.body;
+  // 次にimg要素を作る
+  const imgElement = document.createElement("img");
+  imgElement.src = imagePath;
   return new Promise((resolve, reject) => {
-    // まずはhtmlのbodyを指定
-    const body = document.body;
-    // 次にimg要素を作る
-    const imgElement = document.createElement("img");
-    imgElement.src = imagePath;
-    // img要素をbodyに追加
-    body.appendChild(imgElement);
+    // 画像を読み込んだら発火
     imgElement.onload = () => {
+      // img要素をbodyに追加
+      body.appendChild(imgElement);
+      //クラス名をimagesに変更
       imgElement.classList.add("images");
+      // 要素を返す
       resolve(imgElement);
     };
+
     imgElement.onerror = () => {
       reject(new Error("Failed to load image"));
     };
   });
-}
+};
 
 // 画像1を読み込ませる
-createImage("images/1_image.jpeg")
+let image = createImage("images/1_image.jpeg")
   .then((imgElement) => {
     console.log(`画像1が読み込まれました`);
     return wait(2).then(() => {
@@ -35,13 +39,14 @@ createImage("images/1_image.jpeg")
     });
   })
   .then(() => {
-    //画像2を読み込ませる
-    return createImage("images/2_image.jpeg");
+    // 画像2を読み込む
+    let image2 = createImage("images/2_image.jpeg");
+    return image2;
   })
-  .then((imgElement) => {
+  .then((imgElement2) => {
     console.log(`画像2が読み込まれました`);
     return wait(2).then(() => {
-      imgElement.style.display = "none";
+      imgElement2.style.display = "none";
     });
   })
   .catch((error) => {
