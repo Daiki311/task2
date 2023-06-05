aac42ebca8d87db15be2a1d77a8149b55f9e4ddb
Daiki311 <hd20020311@gmail.com>
2023-06-05 18:31:57 +0900

    2秒待機するメソッドに非表示の追加、画像パスの修正

--- script.js
+++ script.js
@@ -1,53 +1,51 @@
-// wait()を使用すると2秒待機
-const wait = (seconds) => {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve();
-    }, seconds * 1000);
-  });
-};
-
 const createImage = (imagePath) => {
   // まずはhtmlのbodyを指定
   const body = document.body;
   // 次にimg要素を作る
   const imgElement = document.createElement("img");
   imgElement.src = imagePath;
+  //クラス名をimagesに変更
+  imgElement.classList.add("images");
   return new Promise((resolve, reject) => {
     // 画像を読み込んだら発火
     imgElement.onload = () => {
       // img要素をbodyに追加
       body.appendChild(imgElement);
-      //クラス名をimagesに変更
-      imgElement.classList.add("images");
       // 要素を返す
       resolve(imgElement);
     };
-    
+
     imgElement.onerror = () => {
       reject(new Error("Failed to load image"));
     };
   });
 };
 
+// wait()を使用すると2秒待機後画像非表示
+const wait = (seconds) => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      const imgElement = document.querySelectorAll(".images");
+      imgElement.forEach((imgElement) => {
+        imgElement.style.display = "none";
+      });
+      resolve();
+    }, seconds * 1000);
+  });
+};
 // 画像1を読み込ませる
-let image = createImage("images/1_image.jpeg")
-  .then((imgElement) => {
-    console.log(`画像1が読み込まれました`);
-    return wait(2).then(() => {
-      imgElement.style.display = "none";
-    });
+const image = "images/1_image.jpeg";
+createImage(image)
+  .then(() => {
+    return wait(2);
   })
   .then(() => {
     // 画像2を読み込む
-    let image2 = createImage("images/2_image.jpeg");
-    return image2;
+    const image = "images/2_image.jpeg";
+    createImage(image);
   })
-  .then((imgElement2) => {
-    console.log(`画像2が読み込まれました`);
-    return wait(2).then(() => {
-      imgElement2.style.display = "none";
-    });
+  .then(() => {
+    return wait(2);
   })
   .catch((error) => {
     console.error(error);
